7b1313a2e688d920cbbd089cc420fa8e9e9ca388
AbraPanda <garciaguillaume69@gmail.com>
2015-03-19 00:20:12 +0100

    Declare Service and use parameters

--- Utils/BreadcrumbService.php
+++ Utils/BreadcrumbService.php
@@ -12,26 +12,44 @@ class BreadcrumbService
 
     private $breadcrumb;
 
+    private $parameters;
+
     public function __construct(ContainerInterface $container)
     {
-
         $this->container = $container;
+        $this->parameters = $container->getParameter("gg_team_breadcrumb");
         $this->breadcrumb = new Breadcrumb();
+        $this->breadcrumb->setClassCSS($this->parameters['list_class']);
     }
 
-    public function addItem()
+    public function addItem($text, $link, $classCss = null, $translateDomain = null)
     {
-
+        $this->breadcrumb->addItem(
+            $text,
+            $link,
+            ($classCss !== null) ? $classCss : $this->parameters["item_class"],
+            ($translateDomain !== null) ? $translateDomain : $this->parameters["translate_domain"]
+        );
     }
 
-    public function prependItem()
+    public function prependItem($text, $link, $classCss = null, $translateDomain = null)
     {
+        $this->breadcrumb->prependItem(
+            $text,
+            $link,
+            ($classCss !== null) ? $classCss : $this->parameters["item_class"],
+            ($translateDomain !== null) ? $translateDomain : $this->parameters["translate_domain"]
+        );
+    }
 
+    public function setClassCSS($classCSS)
+    {
+        $this->breadcrumb->setClassCSS($classCSS);
     }
 
     public function clear()
     {
-
+        $this->breadcrumb->clear();
     }
 
     /**
